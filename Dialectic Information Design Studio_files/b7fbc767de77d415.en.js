b1b5886aba663ca8447a82eefde506ea03eebffa
dialectic-lab <chandnideadwyler@gmail.com>
2025-07-01 21:39:29 -0400

    Restarting

---
+++ Dialectic Information Design Studio_files/b7fbc767de77d415.en.js
@@ -0,0 +1,2 @@
+window['cmsg']['assets'] = window['cmsg']['assets'] || {};
+window['cmsg']['assets']["en"] = Object.assign(window['cmsg']['assets']["en"] || {}, {"15706":{"js":["820130b085651244.strings.js"],"css":[]},"38701":{"js":["232f9919908d401c.strings.js"],"css":[]}});
